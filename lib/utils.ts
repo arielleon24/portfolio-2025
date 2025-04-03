599c101c14c473285232d972b341c4416db1cd3d
Ariel Leon Flores <arielleon24@gmail.com>
2025-04-03 19:10:31 -0400

    buttonVariants

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,8 @@
 import {type ClassValue, clsx } from 'clsx';
 import {twMerge} from 'tailwind-merge';
 
+// receives className values as ...inputs the uses clsx to combine them togeter. 
+// This is then passed to twMerge to resolve possible conflicts between Tailwind classes.
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
 }
